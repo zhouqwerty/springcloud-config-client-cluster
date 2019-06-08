57b85d95334ff2da115bc6dae7ebc7d154e4db3a
zhou <2891128968@qq.com>
2019-06-08 19:18:34 +0800

    Initial commit

---
+++ client2/src/main/java/com/qwerty/client2/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.qwerty.client2.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RefreshScope
+public class TestController {
+
+    @Value("${test}")
+    private String config;
+
+    @RequestMapping("/test")
+    public String test() throws Exception{
+        return config;
+    }
+}
